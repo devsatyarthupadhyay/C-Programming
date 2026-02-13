2c916330d6358942a0723cbe008b8247c2b3c82a
Satyarth Upadhyay <satyarthupadhyay31@gmail.com>
2026-02-13 19:39:30 +0530

    Nested if else , number is divisible by both 3 and 5 or not

---
+++ If_Else/NestedDivisible5and3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+
+    int n ;
+    printf("Enter the number:");
+    scanf("%d",&n);
+
+    if(n%5==0){
+        if(n%3==0){
+            printf("Given number is divisible by both 3 and 5 ");
+        }
+        else{
+            printf("Given number is not divisible by both 3 and 5");
+        }
+    }
+    return 0;
+}
