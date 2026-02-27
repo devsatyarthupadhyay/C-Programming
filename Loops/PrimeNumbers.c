bdb2d1c82a15e07a848c3a2590d09229087549d1
Satyarth Upadhyay <satyarthupadhyay31@gmail.com>
2026-02-27 22:59:41 +0530

    Prime number

---
+++ Loops/PrimeNumbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    int a=0;
+    for(int i=2;i<=n-1;n++){
+        if(n%i==0){
+            a=1;
+            break;
+        }
+    }
+    if(a==0)printf("The given number is prime");
+    else if("The given number is composite");
+    return 0;
+}
