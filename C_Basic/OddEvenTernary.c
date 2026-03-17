6a1582eac3a2f32083b5d091137f3e59d627357b
Satyarth Upadhyay <satyarthupadhyay31@gmail.com>
2026-03-17 21:55:19 +0530

    Odd or Even Using Ternary

---
+++ C_Basic/OddEvenTernary.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+   int a;
+   printf("Enter number:");
+   scanf("%d",&a);
+   (a % 2 == 0) ? printf("%d is Even \n", a) : printf("%d is Odd \n", a);
+
+   return 0;
+}
