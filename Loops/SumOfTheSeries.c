dcaabd565894e43c6c71f5651b59073078a6c924
Satyarth Upadhyay <satyarthupadhyay31@gmail.com>
2026-03-19 21:30:11 +0530

    Sum of 1-2+3-4+5-6.......n terms

---
+++ Loops/SumOfTheSeries.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the Number of terms you want:");
+    scanf("%d",&n);
+    int sum=0;
+    for(int i=1;i<=n;i++){
+        if(i%2!=0)
+        sum=sum+i;
+        else
+        sum=sum-i;
+    }
+    printf("The sum of the numbers is :%d",sum);
+    return 0;
+}
