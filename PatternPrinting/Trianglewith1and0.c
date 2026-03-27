9f2fb7373d19e2a07434d7549fea10b1a6ad7edc
Satyarth Upadhyay <satyarthupadhyay31@gmail.com>
2026-03-27 22:59:53 +0530

    Triangle made with 0 and 1

---
+++ PatternPrinting/Trianglewith1and0.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+int row;
+printf("Enter number of rows : ");
+scanf("%d",&row);
+int a;
+for(int i=1;i<=row;i++){
+    if(i%2!=0) a=1;
+    else a=0;
+    for( int j =1 ;j<=i;j++){
+        printf("%d",a);
+        if(a==0) a=1;
+        else a=0;
+    }
+    printf("\n");
+}
+    return 0;
+}
