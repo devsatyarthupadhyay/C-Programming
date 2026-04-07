6a2c03f8b6c20a15781ce8f41a60411b2b8f88d3
Satyarth Upadhyay <Satyarthupadhyay31@gmail.com>
2026-04-07 20:21:20 +0530

    Functions

---
+++ Functions/Basics.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void greet(){
+    printf("Good morning \n");
+    printf("How are You \n");
+    return;
+}
+
+int main(){
+    greet();
+    greet();
+    greet();
+    return 0;
+}
